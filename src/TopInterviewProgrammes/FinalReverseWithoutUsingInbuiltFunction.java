07ab153fb8edcf535d60c7d35519e8f0e9ee4250
Paravathi Ganisetti <parvathideveloper@gmail.com>
2022-01-24 19:22:42 +0530

    InterviewProgrammes

---
+++ src/TopInterviewProgrammes/FinalReverseWithoutUsingInbuiltFunction.java
@@ -0,0 +1,11 @@
+package TopInterviewProgrammes;
+
+public class FinalReverseWithoutUsingInbuiltFunction {
+    public static void main(String[] args) {
+        String str = "Venkatesh Ganisetti";
+        char[] chars = str.toCharArray();  // converted to character array and printed in reverse order
+        for (int i = chars.length - 1; i >= 0; i--) {
+            System.out.print(chars[i]);
+        }
+    }
+}
